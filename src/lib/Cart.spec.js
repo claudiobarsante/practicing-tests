64eb42c1370d03a2eeec38f3b4464e7000c5a42f
Claudio Barsante <clbmribas@gmail.com>
2021-05-31 16:19:44 -0300

    checkout() with toMatchSnapshot

--- src/lib/Cart.spec.js
+++ src/lib/Cart.spec.js
@@ -1,6 +1,6 @@
 import Cart from './Cart';
 
-describe('getTotal()', () => {
+describe('Cart', () => {
   let cart;
 
   let product = {
@@ -17,48 +17,90 @@ describe('getTotal()', () => {
     cart = new Cart();
   });
 
-  it('should return 0 when getTotal is executed in a new created instance', () => {
-    expect(cart.getTotal()).toEqual(0);
+  describe('getTotal()', () => {
+    it('should return 0 when getTotal is executed in a new created instance', () => {
+      expect(cart.getTotal()).toEqual(0);
+    });
+
+    it('should multiply quantity and price and receive the total amount', () => {
+      const item = {
+        product,
+        quantity: 2 //70776
+      };
+
+      cart.add(item);
+
+      expect(cart.getTotal()).toEqual(70776);
+    });
+
+    it('should ensure no more than on product exists at a time', () => {
+      cart.add({
+        product,
+        quantity: 2
+      });
+
+      cart.add({
+        product,
+        quantity: 1
+      });
+
+      expect(cart.getTotal()).toEqual(35388);
+    });
+
+    it('should update total when a product gets included and then removed', () => {
+      cart.add({
+        product,
+        quantity: 2
+      });
+
+      cart.add({
+        product: product2,
+        quantity: 1
+      });
+
+      cart.remove(product);
+
+      expect(cart.getTotal()).toEqual(41872);
+    });
   });
 
-  it('should multiply quantity and price and receive the total amount', () => {
-    const item = {
-      product,
-      quantity: 2 //70776
-    };
+  describe('checkout()', () => {
+    it('should return an object with the total and the list of items', () => {
+      cart.add({
+        product,
+        quantity: 2
+      });
 
-    cart.add(item);
+      cart.add({
+        product: product2,
+        quantity: 3
+      });
 
-    expect(cart.getTotal()).toEqual(70776);
-  });
-
-  it('should ensure no more than on product exists at a time', () => {
-    cart.add({
-      product,
-      quantity: 2
+      //to use toMatchInlineSnapshot() must have Prettier installed on project
+      /*expect(cart.checkout()).toMatchInlineSnapshot(`
+        Object {
+          "items": Array [
+            Object {
+              "product": Object {
+                "price": 35388,
+                "title": "Adidas running shoes - men",
+              },
+              "quantity": 2,
+            },
+            Object {
+              "product": Object {
+                "price": 41872,
+                "title": "Adidas running shoes - womens",
+              },
+              "quantity": 3,
+            },
+          ],
+          "total": 196392,
+        }
+      `);*/
+      // -- other alternative is to use toMatchSnapshot() and the snapshot will be
+      // -- stored on the __snapshot__ folder. To refresh snapshot, type 'u' on tests
+      expect(cart.checkout()).toMatchSnapshot();
     });
-
-    cart.add({
-      product,
-      quantity: 1
-    });
-
-    expect(cart.getTotal()).toEqual(35388);
-  });
-
-  it('should update total when a product gets included and then removed', () => {
-    cart.add({
-      product,
-      quantity: 2
-    });
-
-    cart.add({
-      product: product2,
-      quantity: 1
-    });
-
-    cart.remove(product);
-
-    expect(cart.getTotal()).toEqual(41872);
   });
 });
