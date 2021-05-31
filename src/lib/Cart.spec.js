0ced21030e35a52ee8d79909fc0188a941aee7a3
Claudio Barsante <clbmribas@gmail.com>
2021-05-31 18:09:21 -0300

    Dinero.js implemented

--- src/lib/Cart.spec.js
+++ src/lib/Cart.spec.js
@@ -17,9 +17,11 @@ describe('Cart', () => {
     cart = new Cart();
   });
 
+  // -- to run just a selected test change it --> fit
   describe('getTotal()', () => {
     it('should return 0 when getTotal is executed in a new created instance', () => {
-      expect(cart.getTotal()).toEqual(0);
+      // -- getAmount() is a method from the library dinero.js
+      expect(cart.getTotal().getAmount()).toEqual(0);
     });
 
     it('should multiply quantity and price and receive the total amount', () => {
@@ -30,7 +32,7 @@ describe('Cart', () => {
 
       cart.add(item);
 
-      expect(cart.getTotal()).toEqual(70776);
+      expect(cart.getTotal().getAmount()).toEqual(70776);
     });
 
     it('should ensure no more than on product exists at a time', () => {
@@ -44,7 +46,7 @@ describe('Cart', () => {
         quantity: 1
       });
 
-      expect(cart.getTotal()).toEqual(35388);
+      expect(cart.getTotal().getAmount()).toEqual(35388);
     });
 
     it('should update total when a product gets included and then removed', () => {
@@ -60,7 +62,7 @@ describe('Cart', () => {
 
       cart.remove(product);
 
-      expect(cart.getTotal()).toEqual(41872);
+      expect(cart.getTotal().getAmount()).toEqual(41872);
     });
   });
 
@@ -101,6 +103,32 @@ describe('Cart', () => {
       // -- other alternative is to use toMatchSnapshot() and the snapshot will be
       // -- stored on the __snapshot__ folder. To refresh snapshot, type 'u' on tests
       expect(cart.checkout()).toMatchSnapshot();
+    });
+
+    it('should return an object with the total and the list of items when summary() is called ', () => {
+      cart.add({
+        product,
+        quantity: 5
+      });
+
+      cart.add({
+        product: product2,
+        quantity: 3
+      });
+
+      expect(cart.summary()).toMatchSnapshot();
+      expect(cart.getTotal().getAmount()).toBeGreaterThan(0);
+    });
+
+    it('should reset the cart when checkout() is called', () => {
+      cart.add({
+        product: product2,
+        quantity: 3
+      });
+
+      cart.checkout();
+
+      expect(cart.getTotal().getAmount()).toEqual(0);
     });
   });
 });
